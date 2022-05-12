0b0a241eb62b5402b3d6987dc8ff4d767d4eb184
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-05-12 14:11:09 +0200

    Update figure1.py

--- G5-main/figure1.py
+++ G5-main/figure1.py
@@ -15,8 +15,8 @@ class LeNet(nn.Module):
         super(LeNet, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 64, kernel_size = 3, stride = 1, padding = 0)
-        self.conv2 = nn.Conv2d(in_channels = 64, out_channels = 32, kernel_size = 3, stride = 1, padding = 0)
-        self.linear1 = nn.Linear(128, 256)
+        self.conv2 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, stride = 1, padding = 0)
+        #self.linear1 = nn.Linear(512, 256)
         self.linear2 = nn.Linear(256, 10)
         self.relu = nn.ReLU()
 
@@ -26,9 +26,9 @@ class LeNet(nn.Module):
         x = self.conv2(x)
         x = self.relu(x)
 
-        #x = x.reshape(x.shape[0], -1)
-        x = self.linear1(x)
-        x = self.relu(x)
+        x = x.reshape(x.shape[0], -1)
+        #x = self.linear1(x)
+        #x = self.relu(x)
         x = self.linear2(x)
         return x
 
