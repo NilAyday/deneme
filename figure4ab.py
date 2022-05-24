89b7b391a92a461883c41f6e2d6e7e91224067bf
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-05-24 14:51:35 +0200

    Update figure4ab.py

--- figure4ab.py
+++ figure4ab.py
@@ -91,7 +91,7 @@ def initialize_weights(m):
         nn.init.normal_(m.weight.data)
         nn.init.constant_(m.bias.data, 0)
 
-num_epochs = 20
+num_epochs = 50
 lr = 0.005
 num_data = 50000
 batch_size = 128
